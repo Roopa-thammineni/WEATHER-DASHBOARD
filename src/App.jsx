4d633943759126c0e49844a7bdd6ef919f03ee5b
Roopa-thammineni <shineythammineni2005.gmail.com>
2025-06-29 17:44:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+function App() {
+  return (
+    <div className='app'>
+      {/* <img src="https://th.bing.com/th/id/OIP.S2WjWFjonZ7C28IHFQtqeQHaE8?w=274&h=183&c=7&r=0&o=7&pid=1.7&rm=3" alt="DESCRIPTION" /> */}
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
+
